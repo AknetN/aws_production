67c8eafdf366d5687a46aa689f42ae24bd4dc3f1
AknetN <aknet52@gmail.com>
2023-08-24 00:22:10 +0200

    unignored .env

--- aws_project/settings.py
+++ aws_project/settings.py
@@ -78,18 +78,39 @@ WSGI_APPLICATION = "aws_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+
+
+DATABASE_CHOICE = os.environ.get('DATABASE_CHOICE', 'sqlite')
+
+if DATABASE_CHOICE == 'sqlite':
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3"
+        }
     }
-}
-
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT", "5432"),
+        },
+    }
+    
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("DB_NAME"),
+#         "USER": os.environ.get("DB_USER"),
+#         "PASSWORD": os.environ.get("DB_PASSWORD"),
+#         "HOST": os.environ.get("DB_HOST"),
+#         "PORT": os.environ.get("DB_PORT", "5432"),
+#     },
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -124,10 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = "collections/static_collection"  #
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "collections/media"
+# STATIC_URL = "/static/"
+# STATIC_ROOT = "collections/static_collection"  #
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = "collections/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -140,3 +161,27 @@ WHITENOISE_STATICFILES_STORAGE = (
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+USE_S3_SETUP = os.environ.get("USE_S3_SETUP", True)
+
+if USE_S3_SETUP:
+    AWS_ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID") # env
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3BotoStorage"
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+    AWS_S3_VERIFY = True
+    AWS_S3_SIGNATURE_NAME = "s3v4"
+    AWS_DEFAULT_ACL = None
+    
+
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_LOCATION = 'collections/static_collection'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+else:
+    STATIC_URL = "/static/"
+    STATIC_ROOT = "collections/static_collection" 
